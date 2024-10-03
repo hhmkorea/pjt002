50b08570a91ff83fbea1e74b389cc62cfb4211aa
hhmkorea <hhmkorea@nate.com>
2024-10-04 00:31:38 +0900

    Setting : gradle, java17

--- src/main/resources/db/query.sql
+++ src/main/resources/db/query.sql
@@ -1,8 +1,8 @@
 -- 사용자 추가 : 유저이름@아이피주소
-create user 'cos'@'%' identified by 'cos1234';
+-- create user 'cos'@'%' identified by 'cos1234';
 -- ON DB이름.테이블명
 -- 권한 설정 : TO 유저이름@아이피주소
-GRANT ALL PRIVILEGES ON *.* TO 'cos'@'%';
+-- GRANT ALL PRIVILEGES ON *.* TO 'cos'@'%';
 -- 데이터베이스 생성
 CREATE DATABASE board CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
 -- 데이터베이스 사용
