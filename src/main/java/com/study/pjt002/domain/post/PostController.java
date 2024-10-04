37d1a8873628d001d2d6610c12a7697f0a45f970
hhmkorea <yuyee81@gmail.com>
2024-10-04 21:07:53 +0900

    리스트 조회 구현

--- src/main/java/com/study/pjt002/domain/post/PostController.java
+++ src/main/java/com/study/pjt002/domain/post/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 /**
  * packageName    : com.study.pjt002.domain.post
  * fileName       : PostController
@@ -41,6 +43,14 @@ public class PostController {
     @PostMapping("/post/save.do")
     public String savePost(final PostRequest params) { // PostRequest : 등록 요청 데이터를 담은 통(class).
         postService.savePost(params);
-        return "redirect:/post/list.do";
+        return "redirect:/post/list.do"; // list.do 호출
+    }
+
+    // 게시글 리스트 페이지
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list"; // src/main/resources/templates/post/list.html
     }
 }
