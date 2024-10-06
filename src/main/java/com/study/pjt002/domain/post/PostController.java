2e618c9f05f623a45a8a3f94bad0c093473777d2
hhmkorea <yuyee81@gmail.com>
2024-10-06 19:49:04 +0900

    + 검색 기능 추가(URLSearchParams, searchType) + 이전 페이지 정보, 이전 검색 정보 유지 기능 추가(queryParams)
    * 버그 수정 내역 *
    messageRedirect.html : kye, status로 쉼표 넣음.
    Whitelabel Error Page : index.html 추가.

--- src/main/java/com/study/pjt002/domain/post/PostController.java
+++ src/main/java/com/study/pjt002/domain/post/PostController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * packageName    : com.study.pjt002.domain.post
@@ -26,6 +29,23 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+
+    // 사용장게 메시지를 전달하고, 페이지를 redirect 한다.
+    private String showMessageAndRedirect(final MessageDto params, Model model) {
+        model.addAttribute("params", params);
+        return "common/messageRedirect";
+    }
+
+    // 쿼리 스트링 parameter를 Map에 담아 반환
+    private Map<String, Object> queryParamsToMap(final SearchDto queryParams) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("page", queryParams.getPage());
+        data.put("recodeSize", queryParams.getRecordSize());
+        data.put("pageSize", queryParams.getPageSize());
+        data.put("keyword", queryParams.getKeyword());
+        data.put("searchType", queryParams.getSearchType());
+        return data;
+    }
 
     // 게시글 작성 페이지
     @GetMapping("/post/write.do")
@@ -77,15 +97,10 @@ public class PostController {
 
     // 게시글 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final Long id, Model model) { // view.html의 deleteForm name="id" 값을 받아서 처리함.
-        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+    public String deletePost(@RequestParam final Long id, final SearchDto queryParams, Model model) { // view.html의 deleteForm name="id" 값을 받아서 처리함.
         postService.deletePost(id);
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, queryParamsToMap(queryParams)); // queryParams: 이전 페이지 정보.
         return showMessageAndRedirect(message, model);
     }
 
-    // 사용장게 메시지를 전달하고, 페이지를 redirect 한다.
-    private String showMessageAndRedirect(final MessageDto params, Model model) {
-        model.addAttribute("params", params);
-        return "common/messageRedirect";
-    }
 }
